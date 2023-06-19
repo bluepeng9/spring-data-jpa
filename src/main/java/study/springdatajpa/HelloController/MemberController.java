7e24aa6531b4cfd9890a82bdf4bf3117e6077330
bluepeng9 <bluepeng9@gmail.com>
2023-06-19 14:50:08 +0900

    feat: Add examples about page

--- src/main/java/study/springdatajpa/HelloController/MemberController.java
+++ src/main/java/study/springdatajpa/HelloController/MemberController.java
@@ -2,9 +2,13 @@ package study.springdatajpa.HelloController;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.springdatajpa.dto.MemberDto;
 import study.springdatajpa.entity.Member;
 import study.springdatajpa.repository.MemberRepository;
 
@@ -25,9 +29,18 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<MemberDto> map = memberRepository.findAll(pageable).map(MemberDto::new);
+        return map;
+    }
+
     @PostConstruct
     public void init() {
         memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
 }
